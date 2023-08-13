b173ed228536b93c37db7aa1752aada6c67b628d
Gregory Szorc <gregory.szorc@gmail.com>
2023-08-12 18:51:03 -0700

    workspace: add trycmd tests for help output
    In preparation for upgrading clap and verifying we didn't break things.

---
+++ debian-repo-tool/tests/cli_tests.rs
@@ -0,0 +1,12 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#[test]
+fn cli_tests() {
+    let cases = trycmd::TestCases::new();
+
+    cases.case("tests/cmd/*.trycmd").case("tests/cmd/*.toml");
+
+    cases.run();
+}
